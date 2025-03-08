5babdd60b0c23a22ea25a8c4e6d9db51ef6fbeeb
russelle roxas <russelleroxas11@gmail.com>
2025-03-09 02:24:24 +0800

    login

---
+++ scripts.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("loginForm").addEventListener("submit", (event) => {
+        event.preventDefault(); // Prevent form submission
+
+        let username = document.getElementById("username").value;
+        let password = document.getElementById("password").value;
+
+        if (username === "russelle" && password === "qwerty") {
+            alert("Login successful");
+        } else {
+            alert("Login failed");
+        }
+    });
+});
